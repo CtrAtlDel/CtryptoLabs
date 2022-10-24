538901813d6fb80ef70aeb587c814a855564ed2c
CtrAtlDelete <kvasovski34@gmail.com>
2022-10-24 23:03:54 +0300

    Add hash generator

---
+++ lab1_python/part_1/parser.py
@@ -0,0 +1,17 @@
+import argparse
+from encoding import Encoding
+from hash_functions import HashFunctions
+
+size_of_string_begin = 10 # Минимальный размер строки
+size_of_string_end = 15 # Максимальный размер строк
+
+def get_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--file', type=str, metavar='file')
+    parser.add_argument('-e', '--encoding', type=Encoding,
+                        default=Encoding.utf8)
+    parser.add_argument('-f', '--functions', type=HashFunctions,
+                        default=HashFunctions.sha256)
+    parser.add_argument('-n', '--number', type=int)
+    parser.add_argument('-o', '--output', type=str)
+    return parser
